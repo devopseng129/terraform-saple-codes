35124671ed97cfbcb3b822ad5c44f02521b3efed
devopseng129 <devopseng129@gmail.com>
2023-04-03 07:37:28 +0000

    added tfvars file

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.0"
-   }
- }
+    }
+  }
 }
 
 provider "aws" {
@@ -12,10 +12,15 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+variable "instance_type" {
+  type = string
+}
+
+
 resource "aws_instance" "my_instance" {
-  ami    = "ami-0fcf52bcf5db7b003"
-#  region = "us-west-2"
-  instance_type = "t2.micro"
+  ami = "ami-0fcf52bcf5db7b003"
+  #  region = "us-west-2"
+  instance_type = var.instance_type
 
   tags = {
     Nam = "vtfrm"
